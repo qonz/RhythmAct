49e9d82d2791b47b7a478f82d21c9c81171e758c
qonz <taishin1109@gmail.com>
2018-04-12 22:28:30 +0900

    ゲーム実行用クラスを追加

---
+++ src/rhythmact/RhythmAct.java
@@ -0,0 +1,34 @@
+package rhythmact;
+
+import densan.s.game.manager.GameManager;
+import game.Game;
+import game.SceneManager;
+/**
+ * このゲームの実行用クラス
+ * @author Taishin
+ *
+ */
+public class RhythmAct extends Game {
+	
+	/**
+	 * 画面の広さ(横幅)
+	 */
+	public static final int FRAME_WIDTH = 1280;
+	/**
+	 * 画面の高さ(縦幅)
+	 */
+	public static final int FRAME_HEIGHT = 720;
+
+	public RhythmAct(SceneManager sceneManager) {
+		super(RhythmActSceneManager.getInstance());
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		GameManager gm = GameManager.getInstance();
+		gm.createFrame(FRAME_WIDTH, FRAME_HEIGHT);
+		
+	}
+
+}
