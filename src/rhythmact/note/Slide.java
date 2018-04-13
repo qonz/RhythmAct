fb21fcf7127237caf89e90a328df3785d1bef0a6
qonz <taishin1109@gmail.com>
2018-04-13 20:56:32 +0900

    ノーツにタップとスライドを追加

---
+++ src/rhythmact/note/Slide.java
@@ -0,0 +1,14 @@
+package rhythmact.note;
+/**
+ * スライドノーツクラス
+ * @author Taishin
+ *
+ */
+public class Slide extends Note {
+
+	public Slide(double x, double y) {
+		super(x, y, "image/flo.png");
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
---
+++ src/rhythmact/note/Tap.java
@@ -0,0 +1,14 @@
+package rhythmact.note;
+/**
+ * 単音ノーツクラス
+ * @author Taishin
+ *
+ */
+public class Tap extends Note {
+
+	public Tap(double x, double y) {
+		super(x, y, "image/dra.png");
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
