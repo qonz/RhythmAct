843e120274dc2f983e01327e9602f656012f64a7
qonz <taishin1109@gmail.com>
2018-04-13 18:17:37 +0900

    タイトル画面になるクラスを追加

---
+++ src/rhythmact/TitleScene.java
@@ -0,0 +1,32 @@
+package rhythmact;
+
+import densan.s.game.drawing.Drawer;
+import game.Scene;
+import game.SceneManager;
+
+public class TitleScene extends Scene {
+
+	public TitleScene(SceneManager sceneManager) {
+		super(sceneManager);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	@Override
+	public void update() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void draw(Drawer d) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void handle() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
