68fa66a245071e8afbaf97127ef0e4ea3f1c8f8c
qonz <taishin1109@gmail.com>
2018-04-13 14:53:20 +0900

    ノーツ譜を生成するクラスを追加

---
+++ src/rhythmact/note/NoteFactory.java
@@ -0,0 +1,23 @@
+package rhythmact.note;
+/**
+ * ノーツ譜を作成するクラス
+ * @author Taishin
+ *
+ */
+public class NoteFactory {
+	
+	/**
+	 * ノーツを生成する
+	 * @param c - 音符
+	 * @param x - ノーツのx座標
+	 * @param y - ノーツのy座標
+	 * @return
+	 */
+	public static Note createNotes(char c, int x, int y){
+		Note note = null;
+		switch(c){
+//		case ' ':note = new *****;break;
+		}
+		return note;
+	}
+}
