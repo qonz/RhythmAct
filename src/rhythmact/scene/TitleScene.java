5331ced048c78c89d6b3bd5f5bf94df71942017f
qonz <taishin1109@gmail.com>
2018-04-13 18:59:02 +0900

    タイトルシーン(テスト用)を追加

--- src/rhythmact/scene/TitleScene.java
+++ src/rhythmact/scene/TitleScene.java
@@ -1,18 +1,23 @@
 package rhythmact.scene;
 
+import java.awt.Color;
+
 import densan.s.game.drawing.Drawer;
+import densan.s.game.manager.GameManager;
 import game.Scene;
 import game.SceneManager;
+import rhythmact.musicscore.MusicScoreManager;
 /**
  * タイトルシーン
  * @author Taishin
  *
  */
 public class TitleScene extends Scene {
-
+	int i;
 	public TitleScene(SceneManager sceneManager) {
 		super(sceneManager);
 		// TODO 自動生成されたコンストラクター・スタブ
+		MusicScoreManager.getInstance().load("test", "testscore01");
 	}
 
 	@Override
@@ -25,6 +30,10 @@ public class TitleScene extends Scene {
 	public void draw(Drawer d) {
 		// TODO 自動生成されたメソッド・スタブ
 		
+		i++;
+		d.setColor(Color.BLACK);
+		d.setFontSize(20);
+		d.drawString(MusicScoreManager.getInstance().getMusicScore().getMusicName(), i*5%GameManager.getInstance().getFrameWidth(), 500);
 	}
 
 	@Override
