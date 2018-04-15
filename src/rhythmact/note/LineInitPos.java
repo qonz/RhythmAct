df7a710119b7609ef7769d1b5f7f52bd3283e0eb
qonz <taishin1109@gmail.com>
2018-04-15 15:47:44 +0900

    譜面上に判定線を置くクラスを追加

---
+++ src/rhythmact/note/LineInitPos.java
@@ -0,0 +1,23 @@
+package rhythmact.note;
+
+import rhythmact.judgement.JudgeLine;
+import rhythmact.judgement.JudgementManager;
+/**
+ * 判定線を譜面上に置く用のクラス
+ * @author Taishin
+ *
+ */
+public class LineInitPos extends Note {
+
+	public LineInitPos(double x, double y) {
+		super(x, y, null);
+		JudgementManager.getInstance().setJudgeLine(new JudgeLine(getY()));
+	}
+
+	@Override
+	public void hitAction() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
