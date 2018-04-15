6a7fc8fea4a1f7e5b39f620e09d0223d1fb9ce59
qonz <taishin1109@gmail.com>
2018-04-15 16:23:30 +0900

    テスト用シーンを更新

--- src/rhythmact/scene/TitleScene.java
+++ src/rhythmact/scene/TitleScene.java
@@ -7,6 +7,7 @@ import densan.s.game.manager.GameManager;
 import game.Scene;
 import game.SceneManager;
 import rhythmact.RhythmActSetting;
+import rhythmact.judgement.JudgementManager;
 import rhythmact.musicscore.MusicScoreManager;
 /**
  * タイトルシーン
@@ -16,8 +17,9 @@ import rhythmact.musicscore.MusicScoreManager;
 public class TitleScene extends Scene {
 
 	private int i;
+	private JudgementManager jManager = JudgementManager.getInstance();
 	private MusicScoreManager msManager = MusicScoreManager.getInstance();
-	
+	private GameManager gameManager = GameManager.getInstance();
 	
 	public TitleScene(SceneManager sceneManager) {
 		super(sceneManager);
@@ -29,17 +31,27 @@ public class TitleScene extends Scene {
 	public void update() {
 		// TODO 自動生成されたメソッド・スタブ
 		i = (i>500)?-2000:i+RhythmActSetting.getInstance().getSpeed();
+		jManager.update();
 		msManager.update();
 	}
 
 	@Override
 	public void draw(Drawer d) {
 		// TODO 自動生成されたメソッド・スタブ	
-		msManager.draw(d, 0, i);
-		d.setColor(Color.BLACK);
-		d.drawLine(0, 600, GameManager.getInstance().getFrameWidth(), 600, 1);
-		d.setFontSize(20);
-		d.drawString(MusicScoreManager.getInstance().getMusicScore().getMusicName(), i*5%GameManager.getInstance().getFrameWidth(), 500);
+		int width = gameManager.getFrameWidth(), height = gameManager.getFrameHeight();
+		int offsetY = height /3 - (int)jManager.getJudgeLine().getY();
+
+		
+		System.out.println("判定線のY:"+offsetY);
+//		msManager.draw(d, 0, i);
+		
+		jManager.draw(d, 0, offsetY);
+		msManager.draw(d, 0, offsetY);
+		
+//		d.setColor(Color.BLACK);
+//		d.drawLine(0, 600, GameManager.getInstance().getFrameWidth(), 600, 1);
+//		d.setFontSize(20);
+//		d.drawString(msManager.getMusicScore().getMusicName(), i*5%gameManager.getFrameWidth(), 500);
 	}
 
 	@Override
